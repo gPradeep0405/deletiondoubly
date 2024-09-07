1b0485ac26a60f2d4937443dd234cbc3e909cc6b
gPradeep0405 <gpradeep0405@gmail.com>
2024-09-07 13:39:35 +0530

    Add files via upload

---
+++ deletion_DLL.py
@@ -0,0 +1,69 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        self.prev = None
+
+
+class CLL:
+    def __init__(self):
+        self.head = None
+        self.temp = None
+
+    def create(self, data):
+        newnode = Node(data)
+        if self.head is None:
+            self.head = newnode
+            self.temp = newnode
+        else:
+            self.temp.next = newnode
+            newnode.prev = self.temp
+            self.temp = self.temp.next
+
+    def display(self):
+        self.temp = self.head
+        while self.temp is not None:
+            print(self.temp.data, end=" ")
+            self.temp = self.temp.next
+        print()
+
+    def delete_at_front(self):
+        self.head = self.head.next
+        self.head.prev=None
+
+    def delete_at_end(self):
+        self.temp = self.head
+        while self.temp.next is not None:
+            self.temp = self.temp.next
+        self.temp.prev.next = None
+
+    def delete_at_mid(self, pos):
+        i = 1
+        self.temp = self.head
+        while i < pos - 1:
+            self.temp = self.temp.next
+            i += 1
+        self.temp.next = self.temp.next.next
+        self.temp.next.prev = self.temp
+
+
+obj = CLL()
+while True:
+    print("1.Create 2.Display 3.Delete At Front 4.Delete At End 5.Delete At Middle 6.Exit")
+    choice = int(input("Enter choice: "))
+    if choice == 1:
+        data = int(input("Enter data: "))
+        obj.create(data)
+    elif choice == 2:
+        obj.display()
+    elif choice == 3:
+        obj.delete_at_front()
+    elif choice == 4:
+        obj.delete_at_end()
+    elif choice == 5:
+        pos = int(input("Enter position: "))
+        obj.delete_at_mid(pos)
+    elif choice == 6:
+        break
+    else:
+        print("Invalid Choice!")
